eeb47b67c25d529babfaf8ea75850408853e08ce
SoyEseJuan <juansuarezalzate@gmail.com>
2022-04-19 11:54:54 +0200

    primer conmit

---
+++ clase1/pages/about.jsx
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout"
+
+export default function about() {
+  return (
+    <Layout
+    title="about | next.js"
+    description="agregue una description"
+    >
+    <h1>About</h1>
+    </Layout>
+  )
+}
+
---
+++ clase1/pages/contact.jsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout"
+
+export default function contact() {
+  return (
+    <Layout
+    title="Contact | next.js"
+    description="agregue una description"
+    >
+        <h1>Contacto</h1>
+    </Layout>
+  )
+}
